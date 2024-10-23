60c1009fea892bdc6781d978e236d34ce77d4b5f
omitina <ndesm20@gmail.com>
2024-10-23 17:13:40 +0400

    refactor actor

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -1,6 +1,33 @@
+using DefaultNamespace;
 using UnityEngine;
+using Zenject;
 
 public class Actor : MonoBehaviour
 {
-    //common logic for player and his evil clone;  
+    private Transform groundCheck;  
+    private Rigidbody2D _rBody;
+    private bool _isGrounded;
+    
+    [Inject] private ReproduceActionService _reproService;
+    
+    protected virtual void Start()
+    {
+        _rBody = GetComponent<Rigidbody2D>();
+        groundCheck = gameObject.transform.Find("GroundCheck");
+        _rBody.constraints = RigidbodyConstraints2D.FreezeRotation; 
+    }
+
+    protected virtual void Jump()
+    {
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, GameplayValues.GroundCheckRadius, 64); 
+        if (_isGrounded)
+        {
+            _rBody.velocity = new Vector2(_rBody.velocity.x, GameplayValues.JumpForce);
+        }
+    }
+
+    protected virtual void Move(float moveInput)
+    {
+        _rBody.velocity = new Vector2(moveInput * GameplayValues.MoveSpeed, _rBody.velocity.y);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,17 @@ using Zenject;
 namespace DefaultNamespace
 {
     
-    public class Player : MonoBehaviour
+    public class Player : Actor
     {
         [Inject] private InputHandler _inputHandler;
         [Inject] private SpawnPoint _spawnPoint;
         [Inject] private ReproduceActionService _reproService;
 
         private Vector2 _currentPlayerPosition; 
-        
-        private Transform groundCheck;  
-        private Rigidbody2D _rBody;
-        private bool _isGrounded;
-        private void Start()
+      
+        protected override void Start()
         {
-            _rBody = GetComponent<Rigidbody2D>();
-            groundCheck = gameObject.transform.Find("GroundCheck");
-            _rBody.constraints = RigidbodyConstraints2D.FreezeRotation; 
+            base.Start();
             
             _inputHandler.OnSpacePressed += Jump;
             _inputHandler.HorizontalInput += Move;
@@ -32,26 +27,22 @@ namespace DefaultNamespace
             _inputHandler.HorizontalInput -= Move;
             _inputHandler.OnRPressed -= MoveToSpawnPoint;
         }
-
-        private void Jump()
-        {
-            _isGrounded = Physics2D.OverlapCircle(groundCheck.position, GameplayValues.GroundCheckRadius, 64); 
-            if (_isGrounded)
-            {
-                _rBody.velocity = new Vector2(_rBody.velocity.x, GameplayValues.JumpForce);
-            }
-            _reproService.LogAction(ActionKind.Jump, null);
-        }
-
-        private void Move(float moveInput)
-        {
-            _rBody.velocity = new Vector2(moveInput * GameplayValues.MoveSpeed, _rBody.velocity.y);
-            _reproService.LogAction(ActionKind.Move, moveInput);
-        }
-
+        
         private void MoveToSpawnPoint()
         {
             transform.position = _spawnPoint.transform.position; 
+        }
+
+        protected override void Jump()
+        {
+            base.Jump();
+            _reproService.LogAction(ActionKind.Jump, null);
+        }
+
+        protected override void Move(float moveInput)
+        {
+            base.Move(moveInput);
+            _reproService.LogAction(ActionKind.Move, moveInput);
         }
     }
 }
